4776133904d8510e6122d71413a5d69a6e2939e6
GalGabbay <gabbaygal@gmail.com>
2024-02-16 13:46:02 +0200

    Render lis

---
+++ js/main.js
@@ -0,0 +1,65 @@
+'use strict'
+
+var gTodos
+createTodos()
+
+
+function onInit() {
+    renderTodos()
+
+}
+
+function renderTodos() {
+
+    const strHTMLs = gTodos.map(todo => `
+    
+    <li>
+
+    <span>${todo.txt}</span>
+    <button>X</button>
+
+    </li>
+
+    
+    ` )
+
+    const elTodoList = document.querySelector('.todo-list')
+    elTodoList.innerHTML = strHTMLs.join('')
+
+}
+
+function createTodos() {
+    gTodos = [
+
+        createTodo('do this'),
+        createTodo('do also that'),
+        createTodo('try This'),
+
+    ]
+}
+
+
+function createTodo(txt) {
+
+    return {
+        id: makeId(),
+        txt,
+        isDone: false,
+
+
+
+    }
+
+
+}
+
+
+function makeId(length = 5) {
+    var id = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        id += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return id
+}
